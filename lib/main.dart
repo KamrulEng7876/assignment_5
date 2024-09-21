eda9604731980dd3658cb5372268d5e1e78be2fe
KamrulEng7876 <kamrulhasansabug786@gmail.com>
2024-09-21 21:58:50 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:assignment_5/widgets/App.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const CrudApp());
+}
---
+++ lib/widgets/App.dart
@@ -0,0 +1,21 @@
+import 'package:assignment_5/screens/product_list_screen.dart';
+
+import 'package:flutter/material.dart';
+
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const ProductListScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey.shade300,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey.shade300,
+        ),
+      ),
+    );
+  }
+}
